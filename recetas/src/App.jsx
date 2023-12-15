ce7f0314c08f6c5d7e9b42d8afff42641c8942a8
4x3l15 <adramirez@escuelasproa.edu.ar>
2023-12-15 14:29:31 -0300

    .

--- recetas/src/App.jsx
+++ recetas/src/App.jsx
@@ -1,18 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './vite.svg';
-
-import './App.css'
+import React from 'react';
+import './App.css';
 
 function App() {
-
   return (
     <div>
-        <h1>RECETAS</h1>
-</div>
+      <h1>RECETAS</h1>
+      <div className="receta">
+        <h2>Nombre de la Receta 1</h2>
+        <p>Ingredientes: Ingrediente 1, Ingrediente 2, Ingrediente 3</p>
+        <p>Pasos: Paso 1, Paso 2, Paso 3</p>
+      </div>
+      <div className="receta">
+        <h2>Nombre de la Receta 2</h2>
+        <p>Ingredientes: Ingrediente A, Ingrediente B, Ingrediente C</p>
+        <p>Pasos: Paso A, Paso B, Paso C</p>
+      </div>
+      {/* Agrega más recetas según sea necesario */}
+    </div>
   );
-
-  
 }
 
-export default App
+export default App;
